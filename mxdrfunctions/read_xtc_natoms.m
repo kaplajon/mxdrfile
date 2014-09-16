53b143588f9876bf1b4b312b858aa9368c71203b
Jon Kapla <jkapl@mmkdmpc.fos.su.se>
2014-09-16 15:44:09 +0200

    Added LGPL license. Changed and added some files accordingly.

--- mxdrfunctions/read_xtc_natoms.m
+++ mxdrfunctions/read_xtc_natoms.m
@@ -1,3 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Copyright 2014 Jon Kapla
+%%
+%% This file is part of xdrfile-matlab.
+%%
+%% xdrfile-matlab is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU Lesser General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% xdrfile-matlab is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU Lesser General Public License for more details.
+%%
+%% You should have received a copy of the GNU Lesser General Public License
+%% along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [status,natoms]=read_xtc_natoms(file)
 % Reads an xtc or trr file and returns natoms
 % 
--- test_xtc.m
+++ test_xtc.m
@@ -1,4 +1,21 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Copyright 2014 Jon Kapla
+%%
+%% This file is part of xdrfile-matlab.
+%%
+%% xdrfile-matlab is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU Lesser General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% xdrfile-matlab is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU Lesser General Public License for more details.
+%%
+%% You should have received a copy of the GNU Lesser General Public License
+%% along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Jon Kapla, 2014-04-16, jon.kapla@mmk.su.se
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% This script tests the read and write of
