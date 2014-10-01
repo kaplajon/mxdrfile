d0129158b0533b07f6ada8d848e8a3662854de16
Jon Kapla <jon@kapla.se>
2014-10-01 10:28:01 +0200

    Added a @mxdrfile class for a more straightforward way to read trajectory files. No writing functionality in the class, use the standalone functions for now.

---
+++ @mxdrfile/mxdrfile.m
@@ -0,0 +1,120 @@
+classdef mxdrfile
+   % USAGE: traj=mxdrfile('file.trr')
+   % USAGE: traj=mxdrfile('file.xtc')
+   % This class creates a trajectory object with methods for reading
+   % properties from a trajectory in xtc or trr format.
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   %% This file is part of mxdrfile.
+   %%
+   %% Copyright © 2014 Jon Kapla. All Rights Reserved.
+   %%
+   %% Redistribution and use in source and binary forms, with or without
+   %% modification, are permitted provided that the following conditions are
+   %% met:
+   %%
+   %% 1. Redistributions of source code must retain the above copyright
+   %%    notice,this list of conditions and the following disclaimer.
+   %%
+   %% 2. Redistributions in binary form must reproduce the above copyright
+   %%    notice, this list of conditions and the following disclaimer in the
+   %%    documentation and/or other materials provided with the distribution.
+   %%
+   %% 3. The name of the author may not be used to endorse or promote products
+   %%    derived from this software without specific prior written permission.
+   %%
+   %% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+   %% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+   %% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+   %% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+   %% DIRECT,INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   %% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+   %% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+   %% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+   %% STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+   %% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   %% POSSIBILITY OF SUCH DAMAGE.
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   properties
+      fname;
+      ftype;
+      fhandle;
+      natoms;
+      status;
+      tstep;%=libpointer('int32Ptr',int32(0)); % Timestep
+      time;%=libpointer('singlePtr',single(0)); % Time in ps
+      lam;%=libpointer('singlePtr',single(0)); % Lambda
+      box;%=libpointer('singlePtr',b); % Box as a 3x3 matrix
+      x;%=libpointer('singlePtr',); % Coordinates as a 3xnatoms matrix
+      v;%=libpointer('singlePtr'); % Velocities as a 3xnatoms matrix
+      f;%=libpointer('singlePtr'); % Forces as a 3xnatoms matrix
+      prec;%=libpointer('singlePtr',single(0)); % XTC file precision
+   end
+   methods 
+      function obj = mxdrfile(fname)
+          % Initialize the trajectory and read the first frame to fill all
+          % possible fields.
+         if nargin < 1
+            error('mxdrfile:InvalidInitialization',...
+               'Must provide a filename.trr or filename.xtc')
+         end
+         %if nargin == 2
+            %Open the second file for writing
+         %   obj.outfile=outfile
+         %   obj.fhandle=calllib('libxdrfile','xdrfile_open',obj.outfile,'w');
+         %end         
+         obj.fname=fname;
+         [pathstr,fname,ext] = fileparts(obj.fname);
+         if(strcmp(ext,'.xtc'))
+	        [obj.status,obj.natoms]=read_xtc_natoms(obj.fname);
+	     elseif(strcmp(ext,'.trr'))
+            [obj.status,obj.natoms]=read_trr_natoms(obj.fname);
+         else
+	        error('Only .trr and .xtc supported!')
+         end
+         catch_xdr_errors(obj.status);
+         
+         %Get the XDRFILEptr object
+         obj.fhandle=calllib('libxdrfile','xdrfile_open',obj.fname,'r');
+         
+         obj.ftype=ext;
+         obj.tstep=libpointer('int32Ptr',int32(0)); % Timestep
+         obj.time=libpointer('singlePtr',single(0)); % Time in ps
+         obj.lam=libpointer('singlePtr',single(0)); % Lambda
+         b(3,3)=single(0);
+         obj.box=libpointer('singlePtr',b); % Box as a 3x3 matrix
+         x(3,obj.natoms)=single(0);
+         obj.x=libpointer('singlePtr',x); % Coordinates as a 3xnatoms matrix
+         obj.v=libpointer('singlePtr',x); % Velocities as a 3xnatoms matrix
+         obj.f=libpointer('singlePtr',x); % Forces as a 3xnatoms matrix
+         obj.prec=libpointer('singlePtr',single(1000)); % default XTC file precision
+         %Read the first frame
+         obj.read
+      end % mxdrfile
+ 
+      function read(obj)
+      %Read the next frame of the trajectory.
+          if(strcmp(obj.ftype,'.trr'))
+              func='read_trr';
+              args={'libxdrfile',func, obj.fhandle, obj.natoms, obj.tstep,...
+                  obj.time, obj.lam, obj.box, obj.x, obj.v, obj.f};
+              obj.status=calllib(args{:});
+          elseif(strcmp(obj.ftype,'.xtc'))
+              func='read_xtc';
+              args={'libxdrfile',func, obj.fhandle,obj.natoms, obj.tstep,...
+                  obj.time, obj.box, obj.x, obj.prec};
+              obj.status=calllib(args{:});
+          end
+              % libxdrfile: read_trr() never returns exdrENDOFFILE but exdrINT instead
+          if(obj.status==4 && obj.natoms~=0)
+	          status=11;
+          end
+          catch_xdr_errors(obj.status);
+      end % read
+      
+      function delete(obj)
+         if(strcmp(class(trajinit.fhandle),'lib.pointer'))
+             [~,obj.fhandle]=calllib('libxdrfile','xdrfile_close',obj.fhandle);
+         end
+      end % delete
+   end   
+end % classdef
