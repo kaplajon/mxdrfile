9a1becf8e47b421bd16969e562a6a8a1592dfa26
Jon Kapla <jon@kapla.se>
2014-10-17 13:55:01 +0200

    Added makefile. Added a script to load the xdrfile library. TODO: Change the test scripts and documentation to reflect this change.

---
+++ Makefile
@@ -0,0 +1,30 @@
+MXDRPATH:=$(shell pwd)/mxdrfunctions
+XDRFILE=xdrfile-1.1.4
+xdrpatch=patch -p0 <fix_buffer_overrun_1.1.4.patch
+headerpatch=patch -p0 < ../headers.patch
+create_thunk=matlab -nosplash -nodisplay -r "loadmxdrfile;quit"
+
+all: mxdrfile
+mxdrfile: xdrfile $(MXDRPATH)/include/xdrfile/patched.txt
+$(MXDRPATH)/include/xdrfile/patched.txt:
+	@cd $(MXDRPATH);\
+		$(headerpatch);\
+		echo "PATCHED" >include/xdrfile/patched.txt;\
+		cd ..;\
+		$(create_thunk) >/dev/null
+xdrfile: $(MXDRPATH)/include/xdrfile/xdrfile.h
+$(MXDRPATH)/include/xdrfile/xdrfile.h:
+	@tar xzvf $(XDRFILE).tar.gz
+	@$(xdrpatch)
+	@cd $(XDRFILE) ;\
+		./configure --enable-shared --prefix=$(MXDRPATH);\
+		$(MAKE) install; $(MAKE) test
+.PHONY : clean
+clean:
+	@rm -r \
+	    $(XDRFILE) \
+	    $(MXDRPATH)/bin \
+	    $(MXDRPATH)/lib \
+	    $(MXDRPATH)/include \
+	    libxdr_thunk.so \
+	    fileheaders.m 
---
+++ loadmxdrfile.m
@@ -0,0 +1,47 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This script loads the xdrfile library.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This file is part of mxdrfile.
+%%
+%% Copyright © 2014 Jon Kapla. All Rights Reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are
+%% met:
+%%
+%% 1. Redistributions of source code must retain the above copyright
+%%    notice,this list of conditions and the following disclaimer.
+%%
+%% 2. Redistributions in binary form must reproduce the above copyright
+%%    notice, this list of conditions and the following disclaimer in the
+%%    documentation and/or other materials provided with the distribution.
+%%
+%% 3. The name of the author may not be used to endorse or promote products
+%%    derived from this software without specific prior written permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+%% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+%% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+%% DIRECT,INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+%% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+%% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+%% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+%% STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+%% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%% POSSIBILITY OF SUCH DAMAGE.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dir0=pwd;
+addpath(genpath(fullfile(dir0,'mxdrfunctions')))
+if exist('xdrfile.h')==2
+    if exist('fileheaders.m')==2
+        loadlibrary('libxdrfile',@fileheaders);
+    else
+        loadlibrary('libxdrfile','xdrfile_xtc.h','addheader',...
+            'xdrfile_trr','addheader','xdrfile','mfilename',...
+            'fileheaders.m','thunkfilename','libxdr_thunk');
+    end
+else
+    disp('libxdrfile is NOT loaded!')
+    disp('Make sure the lib and headerfiles are in matlabs path!')
+end
